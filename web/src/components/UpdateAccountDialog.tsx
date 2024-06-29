cstogmuller/galaxroom-memos

commit 03feb74bed520dcd8e6a67a393cfef8994fe5b86

Remove ability to change username (frontend)

web/src/components/UpdateAccountDialog.tsx
@@ -97,15 +97,8 @@ const UpdateAccountDialog: React.FC<Props> = ({ destroy }: Props) => {
   };
 
   const handleSaveBtnClick = async () => {
-    if (state.username === "") {
-      toast.error(t("message.fill-all"));
-      return;
-    }
-
     try {
       const updateMask = [];
-      if (!isEqual(currentUser.name.replace(UserNamePrefix, ""), state.username)) {
-        updateMask.push("username");
       }
       if (!isEqual(currentUser.nickname, state.nickname)) {
         updateMask.push("nickname");
@@ -122,7 +115,6 @@ const UpdateAccountDialog: React.FC<Props> = ({ destroy }: Props) => {
       await userStore.updateUser(
         UserPb.fromPartial({
           name: currentUser.name,
-          username: state.username,
           nickname: state.nickname,
           email: state.email,
           avatarUrl: state.avatarUrl,

commit 1ec95e817448043f7e1d487e12b7a5a546d62359

Fix syntax error caused in 03feb74b

web/src/components/UpdateAccountDialog.tsx
@@ -99,7 +99,6 @@ const UpdateAccountDialog: React.FC<Props> = ({ destroy }: Props) => {
   const handleSaveBtnClick = async () => {
     try {
       const updateMask = [];
-      }
       if (!isEqual(currentUser.nickname, state.nickname)) {
         updateMask.push("nickname");
       }